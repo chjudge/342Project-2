4fa10be7e6aa744b11223474f1e970371988d0c4
chjudge <63122915+chjudge@users.noreply.github.com>
2021-05-04 02:44:50 -0400

    basic exchange working

--- src/clientHTTP.java
+++ src/clientHTTP.java
@@ -1,5 +1,66 @@
+/** Author:  Clayton Judge
+  * Course:  COMP 342 Data Communications and Networking
+  * Date:    5 May 2021
+  * Description: Client side of an HTTP application
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class clientHTTP {
+    private static String host;
+    private static String path = null;
+    private final static int port = 80;
     public static void main(String[] args) {
-        
+        //return if no host specified
+        if (args.length < 1) {
+            System.err.println("Error: insufficient command line arguments");
+            return;
+        }
+        //get host and file path from command line argument
+        host = args[0];
+        if (args.length > 1) {
+            path = args[1];
+        }
+        try {
+            //create a socket that connects to the server on port 80
+            Socket mySocket = new Socket(host, port);
+
+            System.out.println("Connection established");
+
+            InputStreamReader streamReader = new InputStreamReader(mySocket.getInputStream());
+
+            BufferedReader reader = new BufferedReader(streamReader);
+
+            PrintWriter writer = new PrintWriter(mySocket.getOutputStream(), true);
+
+            writer.println("GET / HTTP/1.1");
+            writer.println("Host: " + host + ":" + port);
+            writer.println("Connection: Close");
+            writer.println();
+
+            System.out.println("response:");
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0;;) {
+                if (reader.ready()) {
+                    while (i != -1) {
+                        i = reader.read();
+                        sb.append((char) i);
+                    }
+                    break;
+                }
+            }
+
+            System.out.println(sb.toString());
+
+
+            mySocket.close();
+        } catch (IOException e) {
+            System.err.println("Error: Connection terminated unexpectedly");
+        }
     }
 }
--- src/serverHTTP.java
+++ src/serverHTTP.java
@@ -1,5 +1,46 @@
+
+/** Author:  Clayton Judge
+  * Course:  COMP 342 Data Communications and Networking
+  * Date:    5 May 2021
+  * Description: Server side of an HTTP application
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class serverHTTP {
+    private final static int port = 80;
+
     public static void main(String[] args) {
-        
+        try {
+            // create a ServerSocket object connected to port 80
+            ServerSocket serverSocket = new ServerSocket(port);
+
+            for (;;) {
+                // This socket is only created when a client connects to this server
+                Socket mySocket = serverSocket.accept();
+
+                InputStreamReader streamReader = new InputStreamReader(mySocket.getInputStream());
+
+                BufferedReader reader = new BufferedReader(streamReader);
+
+                PrintWriter writer = new PrintWriter(mySocket.getOutputStream());
+
+                String line = reader.readLine();            
+                while (!line.isEmpty()) {
+                    System.out.println(line);
+                    line = reader.readLine();
+                }
+
+                // mySocket.close();
+            }
+
+            // serverSocket.close();
+        } catch (IOException e) {
+            System.err.println("Error: Connection terminated unexpectedly");
+        }
     }
 }
