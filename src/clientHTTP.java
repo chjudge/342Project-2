236adaf3a2bb190575aaddcb0da5cddaf1682ab2
LOOCKMI19 <LOOCKMI19@5CD92537CX.GCC.edu>
2021-05-04 22:00:24 -0400

    fixed imports to get rid of unnecesary stuff

--- src/clientHTTP.java
+++ src/clientHTTP.java
@@ -5,9 +5,7 @@
 */
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
