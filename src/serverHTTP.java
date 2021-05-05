fad38f1cfcfa07445e902a82f8a2b54aaf38b169
LOOCKMI19 <LOOCKMI19@5CD92537CX.GCC.edu>
2021-05-05 11:14:43 -0400

    don't take this shit

--- src/serverHTTP.java
+++ src/serverHTTP.java
@@ -1,3 +1,4 @@
+
 /** Author:  Clayton Judge
   * Course:  COMP 342 Data Communications and Networking
   * Date:    5 May 2021
@@ -13,6 +14,7 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Date;
@@ -24,15 +26,18 @@ public class serverHTTP {
     private static String fileName = "";
 
     public static void main(String[] args) {
+        ServerSocket serverSocket = null;
         try {
             // create a ServerSocket object connected to port 80
-            ServerSocket serverSocket = new ServerSocket(PORT);
-            System.out.println(CURRENT_DIRECTORY.toString());
+            serverSocket = new ServerSocket(PORT);
+            // stop listening for connections after 30 seconds
+            serverSocket.setSoTimeout(30000);
 
             while (true) {
                 System.out.println("\nListening for connection on port " + PORT + "...");
                 // This socket is only created when a client connects to this server
                 Socket socket = serverSocket.accept();
+                System.out.println("Connected to a client\n");
 
                 // read data over the socket
                 InputStreamReader streamReader = new InputStreamReader(socket.getInputStream());
@@ -41,48 +46,45 @@ public class serverHTTP {
                 OutputStream outStream = socket.getOutputStream();
                 PrintWriter writer = new PrintWriter(outStream, true);
 
-                //get file name from the request
+                System.out.println("Getting request from client...");
+                // get file name from the request
                 String line = reader.readLine();
                 if (line.startsWith("GET")) {
                     fileName = line.substring(line.indexOf('/'), line.lastIndexOf("HTTP") - 1);
                     if (fileName.trim().equals("/")) {
                         fileName = "/index.html";
                     }
-                    fileName = fileName.substring(1);
                 }
                 // read in HTTP request
                 while (!line.isEmpty()) {
-                    System.out.println(line);
                     line = reader.readLine();
                 }
 
-                System.out.println("\nSending response...");
+                System.out.println("Sending response...");
 
-                // send requested file
-                File file = new File(fileName);
+                // open requested file
+                File file = new File(CURRENT_DIRECTORY + fileName);
 
                 StringBuilder sb = new StringBuilder(8096);
 
-                if (file.exists()) {
+                // build HTTP responese message
+                if (file.exists() && !file.isDirectory()) {
                     sb.append("HTTP/1.1 200 OK" + END);
                 } else {
-                    sb.append("HTTP/1/1 404 Not Found" + END);
+                    sb.append("HTTP/1.1 404 Not Found" + END);
                 }
                 sb.append("Date: " + new Date() + END);
                 sb.append("Server: my custom server :)" + END);
-                
                 sb.append("Connection: closed" + END);
                 sb.append(END);
 
-                System.out.println(sb.toString());
-
+                // send the message to the client
                 writer.write(sb.toString());
                 writer.flush();
 
-                sb.delete(0, sb.length());
-
                 // send file to client
-                if (file.exists()) {
+                if (file.exists() && !file.isDirectory()) {
+                    System.out.println("Sending requested file to client...");
                     try {
                         FileInputStream fileStream = new FileInputStream(file);
 
@@ -91,7 +93,6 @@ public class serverHTTP {
                         int bytes;
                         // add bytes to the buffer until the end of the file is reached
                         while ((bytes = fileStream.read(buffer)) != -1) {
-                            // System.out.write(buffer, 0, bytes);
                             outStream.write(buffer, 0, bytes);
                             outStream.flush();
                         }
@@ -103,7 +104,8 @@ public class serverHTTP {
                         return;
                     }
                 } else {
-                    //send 404 page
+                    System.out.println("Sending 404 page to client...");
+                    // send 404 page
                     sb.append("<!DOCTYPE html>" + END);
                     sb.append("<html>" + END);
                     sb.append("<head>" + END);
@@ -111,13 +113,12 @@ public class serverHTTP {
                     sb.append("</head>" + END);
                     sb.append("<body>" + END);
                     sb.append("<h1>Not Found</h1>" + END);
-                    sb.append("<p>The requested URL /" + fileName + " was not found on this server.</p>" + END);
+                    sb.append("<p>The requested URL " + fileName + " was not found on this server.</p>" + END);
                     sb.append("<body>" + END);
                     sb.append("</html>" + END);
 
                     writer.write(sb.toString());
 
-                    
                 }
 
                 System.out.println("Done responding to client...");
@@ -128,10 +129,16 @@ public class serverHTTP {
                 reader.close();
                 socket.close();
             }
-
-            // serverSocket.close();
+        } catch (SocketTimeoutException e) {
+            System.err.println("Error: Connection timed out");
         } catch (IOException e) {
             System.err.println("Error: Connection terminated unexpectedly");
+        } finally {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
